4bbd89ab2c4ba852d0a7d235dd8ab9308ee5a922
andre.bockes@ur.de <andre.bockes@ur.de>
2021-04-19 16:10:33 +0200

    02.AB_minorComments

--- scripts/02.softmax_rdm.py
+++ scripts/02.softmax_rdm.py
@@ -18,15 +18,17 @@ from scipy.spatial.distance import squareform
 import seaborn as sns
 import matplotlib
 import matplotlib.pyplot as plt
-%matplotlib qt
+%matplotlib qt # AB: Please comment on here, as this was creating some problems while running the code as far as I can remember
 
 #%% Import custom utils
 import sys, importlib
 sys.path.insert(0, '..')
+# AB: First use of utilis. What is in there, What needed for?
 import utils
 importlib.reload(utils) # Reload If modified during runtime
 
 # %% Load full accuracy dictionary extracted w/ ResNet50-MiTv1
+# AB: extracted with '01.mif_extraction_exploratory.ipynb' or '01.mif_extraction_short.ipynb'
 path_prefix = Path().parent.absolute()
 dict_path = Path('../saved/full/accuracies_per_category_full_mitv1.pkl')
 # Load from file
@@ -37,6 +39,7 @@ accuracies_per_category = pickle.load(f)
 # Compute softmax feature vectors
 ################################################################################
 # Load categories from txt file
+# AB: Where did we get these from initially? (source)
 l_categories = utils.load_categories(Path('../labels/category_momentsv1.txt'))
 
 features_softmax, l_labels = utils.extract_features(l_categories=l_categories,
@@ -48,16 +51,17 @@ print(df_features[:5])
 
 #%% Save to DataFrame to csv
 df_features.to_csv(Path('../outputs/RN50_features_softmax_average_nammed.csv'))
+# AB: Why necessarsy / helpful?
 
 #%%#############################################################################
 #  RDM computation
 ################################################################################
-metric = 'cosine' #'euclidean'
+metric = 'cosine' #'euclidean' # AB: Any other possible? (why cosine used insted of others, etc.)
 rdm_original = squareform(pdist(df_features, metric=metric))
 print(rdm_original.shape)
 
 #%% Plot RDM
-# Set up the matplotlib figur
+# Set up the matplotlib figure
 f, ax = plt.subplots(figsize=(11, 9), num='RDM')
 
 # Draw the heatmap with the mask and correct aspect ratio
