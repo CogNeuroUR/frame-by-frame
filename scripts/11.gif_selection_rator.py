1d827340f49b1f9e4fcedbf8e7ac36868406a249
andre.bockes@ur.de <andre.bockes@ur.de>
2021-04-21 14:27:47 +0200

    11b.AB_minorComments

--- scripts/11.gif_selection_rator.py
+++ scripts/11.gif_selection_rator.py
@@ -8,16 +8,17 @@ import os
 from pathlib import Path
 
 #%% Define input paths
+# change path if needed
 path_gifs = Path('../data/TRIMMING/INPUT/MIT_GIFs_25FPS_480x360p_1.0s_TOP-3-PER-CAT_old+new')
 
-#%% Sweep through input gifs set
+#%% Sweep through input gifs set # AB: why?
 l_gifs = []
 l_missing = []
 for path, subdirs, files in os.walk(path_gifs):
   for name in files:
     if name[-3:] == 'gif':
       l_gifs.append([path.split('/')[-1],   # category
-                       name])                 # file name
+                       name])               # file name
     else:
       print('Ignored: ', name)
       
@@ -32,17 +33,20 @@ print('Total nr. of GIFs: ', len(l_gifs))
 
 #%% Load Rator distribution
 import pandas as pd
+# rator distribution stored in .csv-file (displays which rator rated which categories)
 path_sheet = Path('../saved/20210305_AB_ratorDistribution_qq1+qq2.csv')
 df_rator = pd.read_csv(path_sheet,
                        skiprows=[0],
                        names=['LK', 'PS', 'LS', 'JS'],
                        delimiter=';',
                        usecols=[0, 1, 2, 3],
-                       nan)
+                      #  nan) # AB: this nan throws an error? remove?
+                       )
 
 print(df_rator)
 
 #%%
+# AB: What happens here? Please comment.
 dict_participants = {}
 for rater in list(df_rator.columns):
   l_b = []
@@ -53,6 +57,7 @@ for rater in list(df_rator.columns):
     path_cat = path_gifs / category
     l_files = [x.stem for x in path_cat.glob('**/*') if x.suffix == '.gif']
 
+    # check file name extensions (indicate position)
     for file_name in l_files:
       if file_name[-2:] == '_b':
         l_b.append(file_name)
@@ -67,7 +72,7 @@ for rater in list(df_rator.columns):
 #%% Check
 flat_list = [item for sublist in list(dict_participants.values()) for item in sublist]
 
-assert sum(flat_list) == len(l_gifs)
+assert sum(flat_list) == len(l_gifs) # check for same length
 
 # %% Plot distribution
 import pandas as pd
@@ -96,4 +101,5 @@ sns.catplot(data = df_distrib,
             y = 'Count',
             hue = 'Participants',
             kind='bar')
+# uncomment for savign plot (pay attention to output path)
 #plt.savefig('plots/gif_distribution_per_participant.pdf')
